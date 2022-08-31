8c3c333dbe26405f36f5accb9a56d203fff72abd
Avatar-J <jummyhassan5@gmail.com>
2022-08-31 04:19:46 -0500

    Changed the UI of the profile page and added an image to it

--- virtual_library/src/Pages/profilepage/profilepage.js
+++ virtual_library/src/Pages/profilepage/profilepage.js
@@ -2,55 +2,47 @@ import React from "react"
 import "./profilepage.css"
 
 export default function Profile(){
+    
     return(
-        <div>
-            <div class = "container">
-                <div class = "top-segment">
-                    <a href="">logout</a>
-                    <a href="">home</a>
+        <div className="container">
+
+            <div className="Left-display">
+               <div>
+                 <img src="./profileIcon.webp" alt=""/>
+               </div>
+            </div>
+
+            <div className="Right-display">
+                <h2>PROFILE</h2>
+
+                <div className="details-container">
+
+                <div className="details det-1">
+                 <div >FIRST NAME</div>
+                 <div >LAST NAME</div>
+                 <div >PROGRAMME</div>
+                 <div>LEVEL</div>
+                 <div>EMAIL</div>
+                </div>
+
+                <div className="details det-2">
+                 <div >Jummai</div>
+                 <div >Alhassan</div>
+                 <div >Computer</div>
+                 <div>300</div>
+                 <div>jummy30@gmail.com</div>
+                               
                 </div>
                 
-
-                <div class ="mini-container">
-                    <div class = "left-card">
-                        <p>hello</p>
-                    </div>
-                    <div class = "right-card">
-                        <div class="Name"><h5>MY PROFILE</h5></div>
-                        <div class = "display">
-                        <div>Full name</div>
-                        <div>Ben</div>
-
-                        </div>
-                        <div class="display">
-                            <div>Email</div>
-                            <div>ben@gmail.com</div>
-
-                        </div>
-                      
-                        <div  class="display">
-                        <div>Programme</div>
-                        <div>Computer Engineering</div>
-
-
-                        </div>
-                        <div  class="display">
-                        <div>Level</div>
-                        <div>100</div>
-
-                        </div>
-                    </div>
-
+               
+                
+               
+                
+                
                 </div>
-
-
-            
                 
             </div>
-            
-        
-           
+
         </div>
     )
-
 }

554b15dd38d6695f19b45187dcf825d25cfa3417
ET-Safo <emmanuelsafo918@gmail.com>
2022-09-05 19:18:16 +0000

    added reset password

---
+++ virtual_library/src/Pages/PasswordChange/ResetPassword.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { StyledContainer,StyledFormArea, StyledFormButton, Avatar, StyledTitle, colors, ButtonGroup, CopyrightText, StyledSubTitle} from "../../Components/Style";
+
+import Logo from './../../Assets/Klogo.png';
+
+import { Form, Formik } from "formik";
+
+import { TextInput } from "../../Components/Form";
+
+import {FiLock} from 'react-icons/fi';
+
+import  * as Yup from 'yup';
+
+import { ThreeDots} from 'react-loader-spinner';
+
+const ResetPassword = () => {
+  return (
+    <StyledContainer>
+        <div>
+        <StyledFormArea>
+        <div style={{display:'flex'}}>
+        <Avatar image={Logo}/>
+          <StyledTitle color='Light black' size={40}>
+            <span style={{font: "normal 36px 'Cookie' cursive", margin: 0}}>Virtual</span><span style={{color:'#e0ac1c',fontFamily: 'Poppins'}}>Library</span>
+          </StyledTitle></div>
+          <StyledSubTitle color='Black' size={35} style={{fontFamily:'Poppins'}}>Reset Password</StyledSubTitle>
+          
+          <Formik
+            initialValues={{
+             password : "",
+             repeatPassword: "",
+              
+            }}
+            validationSchema={
+              Yup.object({
+                password: Yup.string()
+                .required("Please enter a password")
+                .min(5, "Password is too short")
+                .required("Please enter a password"),
+                repeatPassword: Yup.string()
+                .required("Confirm Password")
+                .oneOf([Yup.ref("password")], "Password is not the same"),
+              })
+            }
+            
+          
+          >
+            {({isSubmitting })=>(
+              <Form>
+                 <TextInput
+                 name="password" 
+                 type="password"
+                 label="Password"
+                 placeholder="Enter your new password" 
+                 icon={<FiLock/>}
+                />
+
+                <TextInput
+                 name="repeatPassword" 
+                 type="password"
+                 label="Confirm Password"
+                 placeholder="Confirm your new password" 
+                 icon={<FiLock/>}
+                />
+
+              
+
+              <ButtonGroup>
+                {!isSubmitting && (
+                 <StyledFormButton type="submit">
+                  Reset
+              </StyledFormButton>
+              )}
+
+              {isSubmitting && (
+                <ThreeDots
+                  color= {colors.theme}
+                  height = {49}
+                  width = {100}
+                />
+              )}
+              </ButtonGroup>
+              </Form>
+            )}
+          </Formik>
+          {/* <ExtraText>
+            Don't have an account? Click <TextLink to="/signup">Signup</TextLink> to register
+          </ExtraText> */}
+          </StyledFormArea>
+          <CopyrightText>All rights reserved &copy;2022</CopyrightText>
+        </div>
+    </StyledContainer>
+  )
+}
+
+export default ResetPassword 

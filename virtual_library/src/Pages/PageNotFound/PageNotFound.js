382389f5e881b4d642117b87d3252b7dd01a3d5b
vicowu21 <85970139+vicowu21@users.noreply.github.com>
2022-09-19 09:25:26 +0000

    modified the 404 page

--- virtual_library/src/Pages/PageNotFound/PageNotFound.js
+++ virtual_library/src/Pages/PageNotFound/PageNotFound.js
@@ -4,11 +4,24 @@ import {Link} from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <div className = "whole">
-      <div className = "Button">
-        <Link className= "link" to="/home"><button>HomePage</button></Link>
-      </div>
-    </div>
+   <div className = "whole">
+    <div className='sub'>
+      <div className='number'>4</div>
+      <div className='number'>0</div>
+      <div className='number'>4</div>
+      {/* <div className = "Button"> */}
+        {/* <Link className= "link" to="/home"><button>HomePage</button></Link> */}
+      {/* </div> */}
+    </div> 
+    
+    
+    
+    <div className='field'>
+      <h2>Opps...looks like you got lost</h2>
+      <Link className= "link" to="/home">Go home</Link>
+
+      </div> 
+  </div>
   )
 }
 
